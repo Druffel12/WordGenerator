5110f045d91100d55e6ee809c2a206b8bea13217
Zach Oriel <Zacharyaaron15@gmail.com>
2018-05-07 15:17:24 -0700

    UI, toggles, and animations

---
+++ Assets/Folder-Zach/Scripts/MenuButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject optionsUI;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        optionsUI.SetActive(true);
+    }
+
+    public void Back()
+    {
+        optionsUI.SetActive(false);
+    }
+}
---
+++ Assets/Folder-Zach/Scripts/SetCategories.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCategories : MonoBehaviour
+{
+    public bool insultsArray, namesArray, xxxArray, memesArray;
+
+    public void InsultToggle()
+    {
+        if (insultsArray == true)
+        {
+            insultsArray = false;
+        }
+        else
+        {
+            insultsArray = true;
+        }
+    }
+
+    public void NamesToggle()
+    {
+        if (namesArray == true)
+        {
+            namesArray = false;
+        }
+        else
+        {
+            namesArray = true;
+        }
+    }
+
+    public void XXXToggle()
+    {
+        if (xxxArray == true)
+        {
+            xxxArray = false;
+        }
+        else
+        {
+            xxxArray = true;
+        }
+    }
+
+    public void MemesToggle()
+    {
+        if (memesArray == true)
+        {
+            memesArray = false;
+        }
+        else
+        {
+            memesArray = true;
+        }
+    }
+
+}
