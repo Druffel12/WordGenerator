2c67c8cef8ecf4341e5a2d27907fdad9f22d021c
Zach Oriel <Zacharyaaron15@gmail.com>
2018-05-07 17:39:26 -0700

    ALL OF THE UI AND SOUNDS

---
+++ Assets/Folder-Zach/Scripts/Stats.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour
+{
+    public AudioSource metalMusic;
+
+    public static int score;
+    public int startScore = 0;
+    public static float time;
+    public float startTime = 30;
+
+    public Text timeText;
+
+    public GameObject gameOverButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        metalMusic.Play();
+        score = startScore;
+        time = startTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time -= 1 * Time.deltaTime;
+
+        time = Mathf.Clamp(time, 0f, Mathf.Infinity);
+
+        timeText.text = string.Format("{0:00.00}", time);
+
+        if (time <= 0)
+        {
+            time = 0;
+            EndGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            score += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            time -= 5;
+        }
+    }
+
+    void EndGame()
+    {
+        metalMusic.Stop();
+        //Time.timeScale = 0;
+        gameOverButton.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
