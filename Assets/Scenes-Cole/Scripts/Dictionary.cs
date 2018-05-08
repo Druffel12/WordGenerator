ec638f6424e96697f2988c868b09741178f01596
Cole Druffel <Druffel12@gmail.com>
2018-05-07 17:30:53 -0700

    Array selection done

--- Assets/Scenes-Cole/Scripts/Dictionary.cs
+++ Assets/Scenes-Cole/Scripts/Dictionary.cs
@@ -11,20 +11,20 @@ public class Dictionary : MonoBehaviour {
     public string[] WordBaseReferences;
     void Start()
     {
-        // total possible adds 55
-        WordBaseGeneral = new string[]//20
+        // total possible adds 59
+        WordBaseGeneral = new string[]//22
         {
          "Shit", "Fuck", "Ass" , "Asshole", "Bitch",//5
          "Dickhead", "Dumbass", "Fuck Boi", "Fuckwit", "Fucknugget",//5
          "Mother Fucker", "Jackass","Suckass", "Lardass",//4
          "Shitcunt", "Shiznit", "Thundercunt",  "MAGA",//4
-         "Wang-Chung", "No U"//2
+         "Wang-Chung", "No U", "Shizno", "Cockbite"//4
         };
 
-        WordBaseNames = new string[]//8
+        WordBaseNames = new string[]//9
         {
          "Dale", "Paul", "Zach", "Cole", "Terry",//5
-         "Levi", "Mom", "Wyatt"//3
+         "Levi", "Mom", "Wyatt", "Knebel" //4
         };
 
         WordBaseSexual = new string[]//9
@@ -33,17 +33,17 @@ public class Dictionary : MonoBehaviour {
           "Dicks", "Penis", "Schlong", "Twat", "Fuck-Titty",//5
         };
 
-        WordBaseReferences = new string[]//18
+        WordBaseReferences = new string[]//19
         {
             "Wubb-Lubba-Dub-Dub", "Han shot first",//2 
             "Wax on wax off", "I find your lack of faith disturbing.",//2
             "Try not. do or do not. there is no try.", "He's dead Jim",//2
             "Oh my god they killed Kenny. You Bastards!", "Bring out your dead",//2
             "My name is Inigo Montoya. You killed my father. Prepare to die!",//1
-            "These aren't the droids you're looking for.",//1
+            "These aren't the droids you're looking for.", "BOY!",//2
             "You maniacs! You blew it up! Oh, damn you! Damn you all to hell!",//1
             "ET phone home", "What... is the air-speed velocity of an unladen swallow?",//2
-            "I'm sorry, Dave. I'm afraid I can't do that.",  "one ring to rule them all",//2
+            "I'm sorry, Dave. I'm afraid I can't do that.",  "One ring to rule them all",//2
             "Now your thinking with portals.", "REDROCKET", "REDRUM"//3 
         };
     }
--- Assets/Scenes-Cole/Scripts/WordGenerator.cs
+++ Assets/Scenes-Cole/Scripts/WordGenerator.cs
@@ -7,17 +7,63 @@ public class WordGenerator : MonoBehaviour
     
     private int ArraySelect;
     private int PhraseSelect;
-    
+    bool Array1;
+    bool Array2;
+    bool Array3;
+    bool Array4;
+    List<int> NoNos;
     //HOTDAWG
 	// Use this for initialization
+
+        // selecting wanted arrays
+    int arraySelect()
+    {
+        int retval = -1;
+        int start = Random.Range(1, 4);
+        
+        while(true)
+        {
+            bool foundNumber = true;
+            for (int i =0; i < NoNos.Count; i++)
+            {
+                if(start == NoNos[i])
+                {
+                    foundNumber = false;
+                    start++;
+                    if(start > 4) { start = 1; }
+                    break;
+                }
+                
+            }
+            if(foundNumber == true)
+            {
+                retval = start;
+                foundNumber = true;
+                break;
+            }
+            
+        }
+
+
+        return retval;
+    }
+
+    //filtering arrays with rand values to find word selected
 	void Start ()
     {
-       int ArraySelect = Random.Range(1,4);
+        NoNos = new List<int>();
+
+        if (Array1) { NoNos.Add(1); }
+        if (Array2) { NoNos.Add(2); }
+        if (Array3) { NoNos.Add(3); }
+        if (Array4) { NoNos.Add(4); }
+
+        int ArraySelect = arraySelect();
         int PhraseSelect = 0;
         switch (ArraySelect)
         {
             case 1:
-                PhraseSelect = Random.Range(0, 20);
+                PhraseSelect = Random.Range(0, 22);
                 break;
             case 2:
                 PhraseSelect = Random.Range(0, 8);
@@ -26,14 +72,8 @@ public class WordGenerator : MonoBehaviour
                 PhraseSelect = Random.Range(0, 9);
                 break;
             case 4:
-                PhraseSelect = Random.Range(0, 18);
+                PhraseSelect = Random.Range(0, 19);
                 break;
         }
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
 	}
 }
