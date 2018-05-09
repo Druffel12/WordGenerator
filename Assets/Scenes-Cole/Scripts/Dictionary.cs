a4129b384591e4bfccf74059d079956a4915d4f0
Zach Oriel <Zacharyaaron15@gmail.com>
2018-05-08 17:30:29 -0700

    All of the sex stuff

--- Assets/Scenes-Cole/Scripts/Dictionary.cs
+++ Assets/Scenes-Cole/Scripts/Dictionary.cs
@@ -11,41 +11,53 @@ public class Dictionary : MonoBehaviour {
     public string[] WordBaseReferences;
     void Start()
     {
-        // total possible adds 66
-        WordBaseGeneral = new string[]//23
+        // total possible adds 205
+        WordBaseGeneral = new string[]
         {
-         "Shit", "Fuck", "Ass" , "Asshole", "Bitch",//5
-         "Dickhead", "Dumbass", "Fuck Boi", "Fuckwit", "Fucknugget",//5
-         "Mother Fucker", "Jackass","Suckass", "Lardass",//4
-         "Shitcunt", "Shiznit", "Thundercunt",  "MAGA",//4
-         "Wang-Chung", "No U", "Shizno", "Cockbite", "Cuck"//5
+         "Shit", "Fuck", "Ass" , "Asshole", "Bitch", "Dipshit", "Douche Nozzle", "Douche Pickle", "Whore",
+         "Dickhead", "Dumbass", "Fuck Boi", "Fuckwit", "Fucknugget", "Douchebag", "Cunt", "Shithead",
+         "Mother Fucker", "Jackass","Suckass", "Lardass", "Dipass", "Cock sucker", "Cock", "Sister Fister",
+         "Shitcunt", "Shiznit", "Thundercunt",  "MAGA", "Wanker", "Pisspot", "Twatwaffle", "Fuck-knuckle",
+         "Wang-Chung", "No U", "Shizno", "Cockbite", "Cuck", "Loser", "Idiot", "Moron", "Gold-digger"
+         
         };
 
-        WordBaseNames = new string[]//12
+        WordBaseNames = new string[]
         {
-         "Dale", "Paul", "Zach", "Cole", "Terry", "Margaret Thatcher",//6
-         "Levi", "Mom", "Wyatt", "Knebel", "Kobey Deer", "Cameron-Zachary"//6
+         "Dale", "Paul", "Zach", "Cole", "Terry", "Margaret Thatcher", "Hyland", "Michael", "Clayton",
+         "Levi", "Mom", "Wyatt", "Knebel", "Kobey Deer", "Tucker", "Quinn", "Myhre", "Trump", "Jason",
+         "Bentjen", "Druffel", "Gallagher", "Gibbons", "Hines", "Harley", "Hinkle", "Bryan", "Walker",
+         "Kaplor", "Evan", "Miller", "Letko", "Mart mart", "Pearson", "Nguyen", "Kimberly", "Cee Jay",
+         "MacFarlane", "Shea", "McAuley", "Oriel", "Rosenberg", "Caleb", "Seay", "Xander", "Thomas",
         };
 
-        WordBaseSexual = new string[]//10
+        WordBaseSexual = new string[]
         {
 
-          "Va-j-j", "Skeet",  "Rimjob", "Skullfuck", "Cucking",//5
-          "Dicks", "Penis", "Schlong", "Twat", "Fuck Titty",//5
+          "Va-j-j", "Skeet",  "Rimjob", "Skullfuck", "Cucking", "Succubus", "Anal", "Anal Fissures", "Anal stretching",
+          "Dicks", "Penis", "Schlong", "Twat", "Fuck Titty", "Dogging", "Cum slut", "Cum bucket", "Anal fisting", "Smut",
+          "Cum Dumpster", "Smegma", "Queefing", "Semen", "Golden Shower", "TRUMP TAPE", "Bukkake", "Futa", "Muff Diving",
+          "Three way", "Four way", "Five way", "Orgy", "Strap-on", "Lewd", "Murloc Bulge", "Lube", "Penis pump", "BDSM",
+          "Water BDSM", "Electric BDSM", "Foot porn", "Porn", "Foot Pussy", "Two Girls One Cup", "Scaley", "Furry", "Loli",
+          "Hentai", "Tentai", "MILF", "GILF", "Virgin", "Cherry", "Choking", "Cream Pie", "Jailbait", "Climax", "Ejaculate",
+          "Orgasm", "Role Play", "Foreplay", "Fingering", "Oral", "Handjob", "Blowjob", "Titjob", "Grinding", "Buttplug",
+          "Masturbation", "Circle Jerk", "Cock Ring", "Cross The Streams", "Eiffel Tower", "Jedi Fight", "Cum Shot", "Intercrucial Sex",
+          "Cowgirl", "Reverse Cowgirl", "Pegging", "69", "Missionary", "Amazonian", "Daddy", "Docking", "Butt Stuff", "Shocker",
+          "Spocker", "Kinky", "Fetlang", "Pennywise", "Double Fisting", "Thanos' Dick", "Ear Fuck", "Karmasutra", "Menajahtwa"
         };
 
         WordBaseReferences = new string[]//21
         {
-            "Wubb-Lubba-Dub-Dub", "Han shot first", "They're taking the hobbits to Isengard!",//3 
-            "Wax on wax off", "I find your lack of faith disturbing.",//2
-            "Do or do not, there is no try.", "He's dead, Jim!",//2
-            "Oh my god they killed Kenny! You Bastards!", "Bring out your dead",//2
-            "My name is Inigo Montoya. You killed my father. Prepare to die!",//1
-            "These aren't the droids you're looking for.", "BOY!",//2
-            "You maniacs! You blew it up! Oh, damn you! Damn you all to hell!",//1
-            "ET phone home", "What... is the air-speed velocity of an unladen swallow?",//2
-            "I'm sorry, Dave. I'm afraid I can't do that.",  "One ring to rule them all",//2
-            "Now you're thinking with portals.", "REDROCKET", "REDRUM", "Praise the sun"//4 
+            "Wubb-Lubba-Dub-Dub", "Han shot first", "They're taking the hobbits to Isengard!",
+            "Wax on wax off", "I find your lack of faith disturbing.",
+            "Do or do not, there is no try.", "He's dead, Jim!", "Seven holes",
+            "Oh my god they killed Kenny! You Bastards!", "Bring out your dead",
+            "My name is Inigo Montoya. You killed my father. Prepare to die!",
+            "These aren't the droids you're looking for.", "BOY!",
+            "You maniacs! You blew it up! Oh, damn you! Damn you all to hell!",
+            "ET phone home", "What... is the air-speed velocity of an unladen swallow?",
+            "I'm sorry, Dave. I'm afraid I can't do that.",  "One ring to rule them all",
+            "Now you're thinking with portals.", "Redrocket", "REDRUM", "Praise the sun"
         };
     }
 }
