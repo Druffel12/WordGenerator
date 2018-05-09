7b738dfb9ed1de55f720472cdc28e566a22ea34b
Zach Oriel <Zacharyaaron15@gmail.com>
2018-05-08 18:01:03 -0700

    Final fixes

---
+++ Assets/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour {
+
+    public static ServiceLocator instance;
+
+    public GameObject toggleOptions;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
